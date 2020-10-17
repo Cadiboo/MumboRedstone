a7abcc4139e91bedce379eb2a6c40b9eb2527086
Cadiboo <37298044+Cadiboo@users.noreply.github.com>
2020-10-17 22:39:30 +1100

    Fix transmitter block not transmitting if it is initially placed next to a powered block

--- src/main/java/com/supercat765/mumboredstone/block/WirelessTransmitterBlock.java
+++ src/main/java/com/supercat765/mumboredstone/block/WirelessTransmitterBlock.java
@@ -7,13 +7,9 @@ import net.minecraft.state.BooleanProperty;
 import net.minecraft.state.StateContainer;
 import net.minecraft.state.properties.BlockStateProperties;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
-import net.minecraft.world.server.ServerWorld;
-
-import java.util.Random;
 
 /**
  * Pretty much a copy of the lever block but without a facing direction
@@ -40,9 +36,10 @@ public class WirelessTransmitterBlock extends Block {
             worldIn.setBlockState(pos, state.func_235896_a_(POWERED), 3);
     }
 
-    /** Copied and modified from the Lamp */
     @Override
-    public void tick(BlockState state, ServerWorld worldIn, BlockPos pos, Random rand) {
+    public void onBlockAdded(BlockState state, World worldIn, BlockPos pos, BlockState oldState, boolean isMoving) {
+        if (worldIn.isRemote)
+            return;
         if (state.get(POWERED) != worldIn.isBlockPowered(pos))
             worldIn.setBlockState(pos, state.func_235896_a_(POWERED), 3);
     }
