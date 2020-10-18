ec073a529c82faedf5fa69a4c91bb9662ae253a9
Cadiboo <37298044+Cadiboo@users.noreply.github.com>
2020-10-18 11:30:27 +1100

    Change up how the receiver works to make it work in any direction

--- src/main/java/com/supercat765/mumboredstone/block/WirelessReceiverBlock.java
+++ src/main/java/com/supercat765/mumboredstone/block/WirelessReceiverBlock.java
@@ -4,11 +4,16 @@ import com.supercat765.mumboredstone.init.MRTileEntityTypes;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.RedstoneDiodeBlock;
+import net.minecraft.state.BooleanProperty;
 import net.minecraft.state.StateContainer;
+import net.minecraft.state.properties.BlockStateProperties;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.world.IWorldReader;
 import net.minecraft.world.World;
 import net.minecraft.world.server.ServerWorld;
 
@@ -17,32 +22,70 @@ import java.util.Random;
 /**
  * Outputs a redstone signal from it's wireless channel.
  */
-public class WirelessReceiverBlock extends RedstoneDiodeBlock {
+public class WirelessReceiverBlock extends Block {
+    protected static final VoxelShape SHAPE = Block.makeCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 2.0D, 16.0D);
+    public static final BooleanProperty POWERED = BlockStateProperties.POWERED;
+
     public WirelessReceiverBlock(Properties properties) {
         super(properties);
-        this.setDefaultState(this.stateContainer.getBaseState().with(HORIZONTAL_FACING, Direction.NORTH).with(POWERED, false));
+        this.setDefaultState(this.stateContainer.getBaseState().with(POWERED, false));
     }
 
     @Override
     protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
-        builder.add(HORIZONTAL_FACING, POWERED);
-    }
-
-    /** Stops it accepting power from normal redstone */
-    @Override
-    protected int calculateInputStrength(World worldIn, BlockPos pos, BlockState state) {
-        return 0;
-    }
-
-    /** Stop it trying to turn itself off because it thinks it's still a diode */
-    @Override
-    public void tick(BlockState state, ServerWorld worldIn, BlockPos pos, Random rand) {
-        // NO OP
+        builder.add(POWERED);
     }
 
     @Override
-    protected int getDelay(BlockState state) {
-        return 0;
+    public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
+        return SHAPE;
+    }
+
+    @Override
+    public boolean isValidPosition(BlockState state, IWorldReader worldIn, BlockPos pos) {
+        return hasSolidSideOnTop(worldIn, pos.down());
+    }
+
+    @Override
+    public boolean canProvidePower(BlockState state) {
+        return true;
+    }
+
+    @Override
+    public int getStrongPower(BlockState blockState, IBlockReader blockAccess, BlockPos pos, Direction side) {
+        return blockState.get(POWERED) ? 15 : 0;
+    }
+
+    @Override
+    public int getWeakPower(BlockState blockState, IBlockReader blockAccess, BlockPos pos, Direction side) {
+        return blockState.get(POWERED) ? 15 : 0;
+    }
+
+    @Override
+    public void neighborChanged(BlockState state, World worldIn, BlockPos pos, Block blockIn, BlockPos fromPos, boolean isMoving) {
+        if (!state.isValidPosition(worldIn, pos)) {
+            TileEntity tileentity = worldIn.getTileEntity(pos);
+            spawnDrops(state, worldIn, pos, tileentity);
+            worldIn.removeBlock(pos, false);
+        }
+    }
+
+    /** Copied and modified from the Lever */
+    @Override
+    public void onReplaced(BlockState state, World worldIn, BlockPos pos, BlockState newState, boolean isMoving) {
+        if (!isMoving && !state.isIn(newState.getBlock())) {
+            if (state.get(POWERED))
+                worldIn.notifyNeighborsOfStateChange(pos, this);
+            super.onReplaced(state, worldIn, pos, newState, isMoving);
+        }
+    }
+
+    @Override
+    public void onBlockAdded(BlockState state, World worldIn, BlockPos pos, BlockState oldState, boolean isMoving) {
+        if (worldIn.isRemote)
+            return;
+        if (state.get(POWERED) != worldIn.isBlockPowered(pos))
+            worldIn.setBlockState(pos, state.func_235896_a_(POWERED), 3);
     }
 
     @Override
